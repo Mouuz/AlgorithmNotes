821fed10c5d17024db415a1ae79c07ce0d4ed477
Mouuz <mouzzlzm91@gmail.com>
2023-01-23 22:20:09 +0800

    add sort

---
+++ 排序/merge_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+void Quick_sort(vector<int> & array)
+{
+
+}
+
+
+int main()
+{
+    vector<int> array {5,8,9,10,1,2,15};
+    Quick_sort(array);
+    cout << array << endl;
+    return 0;
+}
