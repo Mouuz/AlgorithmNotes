4d02c2424ed15d817b8eaab6ce1ac10e8646b4ad
Mouuz <mouzzlzm91@gmail.com>
2023-02-08 00:00:02 +0800

    update quicksort mergesort

--- 排序/merge_sort.cpp
+++ 排序/merge_sort.cpp
@@ -5,16 +5,39 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-void Quick_sort(vector<int> & array)
-{
+int n;
+vector<int> temp(n, 0);
 
+void Merge_sort(vector<int> & arr, int l, int r)
+{
+    if(l >= r) return;
+    int mid = l + (r - l >> 1);
+    Merge_sort(arr, l, mid);
+    Merge_sort(arr, mid + 1, r);
+
+    int i = l, j = mid + 1, k = 0;
+    while(i <= mid && j <= r){
+        if(arr[i] < arr[j]) temp[k++] = arr[i++];
+        else temp[k++] = arr[j++];
+    }
+    while(i <= mid) temp[k++] = arr[i++];
+    while(j <= r) temp[k++] = arr[j++];
+
+    for(i = l, j = 0; i <= r; ++i, ++j) arr[i] = temp[j]
 }
 
 
 int main()
 {
-    vector<int> array {5,8,9,10,1,2,15};
-    Quick_sort(array);
-    cout << array << endl;
+    scanf("%d", &n);
+    vector<int> arr(n, 0);
+    for(int i = 0; i < n; ++i)
+    {
+        scanf("%d", &arr[i]);
+    }
+    Quick_sort(arr, 0 , n-1);
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
